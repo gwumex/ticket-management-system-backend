851c695270069008704a83c36d3e00abb9de575c
gwumex <42099116+gwumex@users.noreply.github.com>
2022-11-30 10:49:47 +0100

    mongoose connect

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const path = require('path');
-const {logger} = require('./middleware/logger')
+const { logger, logEvents } = require('./middleware/logger')
+const errorhandler = require('./middleware/errorHandler');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions')
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3500;
 
-
+console.log(process.env.NODE_ENV);
+connectDB();
 app.use(logger)
+app.use(cors(corsOptions))
 app.use(express.json())
+app.use(cookieParser())
 app.use('/', express.static(path.join(__dirname, '/public')))
 app.use('/', require('./routes/root'));
-
 app.all('*', (req, res) => {
     res.status(404)
     if ( req.accepts('html')){
@@ -20,6 +29,18 @@ app.all('*', (req, res) => {
        res.type('txt').send('404 Not found')
     }
 })
-app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`);
+
+app.use(errorhandler);
+
+mongoose.connection.once('open', () => {
+    console.log("Connected to MongoDb");
+    app.listen(PORT, () => {
+        console.log(`server running on port ${PORT}`);
+    })
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err);
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 
+    'mongoErrLog.log')
 })
